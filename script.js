0e5b6b00dc0332840f65eab7581213d249c1390d
Brenna <brennabruening@gmail.com>
2021-03-28 15:22:20 -0400

    About me paragraph formatting updates

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const icecreamCone;
+const aLaMode = document.getElementById("alamode");
+
+aLaMode.addEventListener("click", function() {
+    alert("with whipped cream & sprinkles!");
+});
+
+/*
+
+aLaMode.addEventListener("mouseover", function(event) {
+    aLaMode.setAttribute("style", "background-color: lightgreen");
+    //event.target.style.color = "green";
+
+    setTimeout(function() {
+        aLaMode.setAttribute("style", "background-color: ");
+    }, 500);
+}, false);
+
+//aLaMode.addEventListener();
+
+*/
+
+
